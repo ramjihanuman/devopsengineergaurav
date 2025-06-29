4cba90089df3cbed15f1356216e8928ebc22c239
gaurav Shukla <gauravshukla2548@gmail.com>
2025-06-29 18:50:04 +0530

    add notes

--- modules/RG/main.tf
+++ modules/RG/main.tf
@@ -1,6 +1,5 @@
 resource "azurerm_resource_group" "rg_todo" {
-    # This resource group is used to contain all resources for the Todo application
-    # It is created in the specified location and with the specified name from variables
+    # mera name joker mai hu dhundu 
     name     = var.resource_group_name
     location = var.resource_group_location
 }
